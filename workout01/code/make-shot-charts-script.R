81b90bff405efebb45ddda88db22d65318bedc1c
Michelle Chuang <mchuang@berkeley.edu>
2019-03-13 19:53:43 -0700

    Finished make-shot-charts-script.R

--- workout01/code/make-shot-charts-script.R
+++ workout01/code/make-shot-charts-script.R
@@ -1,8 +1,66 @@
 library(readr)
 library(ggplot2)
+library(grid)
+library(jpeg)
 
+andre <- read_csv('../data/andre-iguodala.csv')
+draymond <- read_csv('../data/draymond-green.csv')
+kevin <- read_csv('../data/kevin-durant.csv')
 klay <- read_csv('../data/klay-thompson.csv')
+stephen <- read_csv('../data/stephen-curry.csv')
 
-klay_scatterplot <- ggplot(data = klay) + geom_point(aes(x = x, y = y, color = shot_made_flag))
+court_file <- "../images/nba-court.jpg"
+court_image <- rasterGrob(
+  readJPEG(court_file),
+  width = unit(1, "npc"),
+  height = unit(1, "npc"))
 
-klay_scatterplot
+klay_shot_chart <- ggplot(data=klay) + annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) + 
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Klay Thompson (2016 season)') +
+  theme_minimal()
+
+andre_shot_chart <- ggplot(data=andre) + annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) + 
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Andre Iguodala (2016 season)') +
+  theme_minimal()
+
+draymond_shot_chart <- ggplot(data=draymond) + annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) + 
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Draymond Green (2016 season)') +
+  theme_minimal()
+
+kevin_shot_chart <- ggplot(data=kevin) + annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) + 
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Kevin Durant (2016 season)') +
+  theme_minimal()
+
+stephen_shot_chart <- ggplot(data=stephen) + annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) + 
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Stephen Curry (2016 season)') +
+  theme_minimal()
+
+pdf(file = '../images/andre-iguodala-shot-chart.pdf', width = 6.5, height = 5)
+andre_shot_chart
+dev.off()
+
+pdf(file = '../images/draymond-green-shot-chart.pdf', width = 6.5, height = 5)
+draymond_shot_chart
+dev.off()
+
+pdf(file = '../images/kevin-durant-shot-chart.pdf', width = 6.5, height = 5)
+kevin_shot_chart
+dev.off()
+
+pdf(file = '../images/klay-thompson-shot-chart.pdf', width = 6.5, height = 5)
+klay_shot_chart
+dev.off()
+
+pdf(file = '../images/stephen-curry-shot-chart.pdf', width = 6.5, height = 5)
+stephen_shot_chart
+dev.off()
